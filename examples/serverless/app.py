6df663acd38a4db929785d8d4f200527eaf0d4b2
dkraczkowski <dawid.kraczkowski@gmail.com>
2020-11-27 22:38:55 +0100

    Moved CI/CD to github

--- examples/serverless/app.py
+++ examples/serverless/app.py
@@ -1,3 +1,27 @@
 from chocs import HttpApplication
+from chocs import HttpRequest
+from chocs import HttpResponse
+from chocs.middleware import Middleware
+from chocs.middleware import MiddlewareHandler
 
-app = HttpApplication()
+
+def cors_middleware(request: HttpRequest, next: MiddlewareHandler) -> HttpResponse:
+    response = next(request)
+    response._headers.set('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+class CorsMiddleware(Middleware):
+    def __init__(self, allowed_access_from: str):
+        self.cors = allowed_access_from
+
+    def handle(self, request: HttpRequest, next: MiddlewareHandler) -> HttpResponse:
+
+        response = next(request)
+        response._headers.set('Access-Control-Allow-Origin', self.cors)
+
+        return response
+
+
+app = HttpApplication(CorsMiddleware('*'))
