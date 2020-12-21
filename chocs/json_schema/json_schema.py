6286a243a726b660c1ab3a4d6137b848074e6f31
dkraczkowski <dawid.kraczkowski@gmail.com>
2020-12-21 11:39:23 +0100

    Refactor for URILoader

--- chocs/json_schema/json_schema.py
+++ chocs/json_schema/json_schema.py
@@ -1,31 +1,33 @@
 from functools import cached_property, partial
 from json import load as json_load
 from os import path
-from typing import Dict, List, Mapping, Optional, Sequence, Union
+from typing import Dict, List, Optional, Union
 
 from yaml import FullLoader as YamlFullLoader, load as yaml_load
 
 yaml_load = partial(yaml_load, Loader=YamlFullLoader)
 
 
-class JsonLoader:
+class URILoader:
     def __init__(self):
         self.store = {}
 
     def load(self, uri):
-        file_reader = FileReader(uri)
-        return file_reader.contents
+        if uri not in self.store:
+            file_reader = FileReader(uri)
+            self.store[uri] = file_reader.contents
+
+        return self.store[uri]
 
 
-_default_json_loader = JsonLoader()
+_default_uri_loader = URILoader()
 
 
 class JsonReference:
-    def __init__(self, uri: str, context: Mapping = {}, _loader: JsonLoader = _default_json_loader):
+    def __init__(self, uri: str, loader: URILoader = _default_uri_loader):
         self.file_name, self.reference = uri.split("#")
-        self.context = context
         self.id = uri
-        self._loader = _loader
+        self._loader = loader
 
     def __getitem__(self, key: str):
         return self.data[key]
@@ -38,11 +40,12 @@ class JsonReference:
             if item not in schema:
                 raise KeyError(f"Could not resolve reference {self.reference}")
             schema = schema[item]
-
+        if isinstance(schema, JsonReference):
+            return schema.data
         return schema
 
     def __repr__(self):
-        return f"JsonReference({self.id}, {repr(self.context)})"
+        return f"JsonReference({self.id})"
 
 
 _default_store = {}
@@ -64,38 +67,27 @@ class FileReader:
     def read(self) -> dict:
         file_handler = open(self.file_name)
         contents = self._loader(file_handler)
+        file_handler.close()
 
-        self._replace_references(contents)
+        return self._replace_references(contents)
 
-        return contents
+    def _replace_references(self, obj):
+        try:
+            if not isinstance(obj["$ref"], str):
+                raise TypeError
+            return self._create_reference(obj)
+        except (TypeError, LookupError):
+            pass
 
-    def _replace_references(self, obj, parent: Optional[Union[Dict, List]] = None, uri: List[Union[str, int]] = []) -> None:
-        if isinstance(obj, List):
-            self._replace_references_in_list(obj, parent, uri)
-        elif isinstance(obj, Dict):
-            self._replace_references_in_dict(obj, parent, uri)
+        if isinstance(obj, list):
+            return [self._replace_references(item) for item in obj]
 
-    def _replace_references_in_dict(self, obj: Dict, parent: Optional[Union[Dict, List]], uri: List[Union[str, int]]) -> None:
-        if "$ref" in obj:
-            if not parent:
-                raise ReferenceError(f"Cannot reference from root path in file {self.file_name}")
+        elif isinstance(obj, dict):
+            return {key: self._replace_references(value) for key, value in obj.items()}
 
-            parent[uri[-1]] = self._create_reference(obj)  # if there is a ref in the object rest is ignored
+        return obj
 
-            return
-
-        for key, value in obj.items():
-            if not isinstance(value, (dict, list)):
-                continue
-            self._replace_references(value, obj, uri + [key])
-
-    def _replace_references_in_list(self, obj: List, parent: Optional[Union[Dict, List]], uri: List[Union[str, int]]) -> None:
-        for key, value in enumerate(obj):
-            if not isinstance(value, (dict, list)):
-                continue
-            self._replace_references(value, obj, uri + [key])
-
-    def _create_reference(self, ref_obj: Mapping) -> JsonReference:
+    def _create_reference(self, ref_obj: dict) -> JsonReference:
         ref_str = ref_obj["$ref"]
         file_part, ref_part = ref_str.split("#")
         if not file_part:
@@ -106,7 +98,7 @@ class FileReader:
         full_ref = file_part + "#" + ref_part
 
         if full_ref not in self.store:
-            self.store[full_ref] = JsonReference(full_ref, context=ref_obj)
+            self.store[full_ref] = JsonReference(full_ref)
 
         return self.store[full_ref]
 
@@ -116,14 +108,14 @@ class FileReader:
 
 
 class OpenApiSchema:
-    def __init__(self, file_name: str):
+    def __init__(self, file_name: str, loader: URILoader = _default_uri_loader):
         self.file_name = file_name
         self.schema: dict = {}
+        self.loader = loader
 
     @cached_property
     def contents(self) -> dict:
-        file = FileReader(self.file_name)
-        return file.contents
+        return self.loader.load(self.file_name)
 
     def __getitem__(self, reference: str):
         reference_path = reference.lstrip("#").strip("/").split("/")
--- tests/json_schema/test_resolve_reference.py
+++ tests/json_schema/test_resolve_reference.py
@@ -7,5 +7,4 @@ def test_can_open_json_schema_file() -> None:
     openapi_file = path.realpath(path.dirname(__file__) + "/../fixtures/openapi.yml")
     schema = OpenApiSchema(openapi_file)
     pet = schema["#/components/schemas/Pet"]
-
     print(pet)
