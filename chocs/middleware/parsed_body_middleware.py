65e3fe2fb01f8e0de360fe6c4fe820c884a45822
dkraczkowski <dawid.kraczkowski@gmail.com>
2021-04-08 16:14:13 +0200

    Lint fixes

--- chocs/middleware/parsed_body_middleware.py
+++ chocs/middleware/parsed_body_middleware.py
@@ -25,7 +25,7 @@ class ParsedBodyMiddleware(Middleware):
         if not inspect.isclass(route.attributes["parsed_body"]):
             return
 
-        if not hasattr(request.parsed_body, '__getitem__') or not hasattr(request.parsed_body, '__iter__'):
+        if not hasattr(request.parsed_body, "__getitem__") or not hasattr(request.parsed_body, "__iter__"):
             return
 
         body = request.parsed_body
