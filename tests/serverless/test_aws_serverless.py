d088d49992b76c8b7c321d10b5c3776ab2ab4fe2
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2022-02-01 16:17:27 +0100

    Allow aws path parameters to be None

--- tests/serverless/test_aws_serverless.py
+++ tests/serverless/test_aws_serverless.py
@@ -135,3 +135,30 @@ def test_content_types_are_not_base64_encoded(content_type) -> None:
 
     assert response["isBase64Encoded"] is False
     assert response["body"] == "/test/123"
+
+
+@pytest.mark.parametrize("aws_event", [
+    "fixtures/lambda_http_api_event.json",
+    "fixtures/lambda_rest_api_event.json",
+])
+def test_can_pass_none_in_path_parameters(aws_event: str) -> None:
+    # given
+    def test_callback(request: HttpRequest) -> HttpResponse:
+        assert request.path_parameters == {}
+        return HttpResponse("OK")
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    event_json = json.load(
+        open(os.path.join(dir_path, "..", aws_event))
+    )
+    event_json["pathParameters"] = None
+    serverless_callback = AwsServerlessFunction(test_callback)
+
+    # when
+    response = serverless_callback(event_json, {})
+
+    # then
+    assert response["statusCode"] == 200
+
+
+
